dcd8cd1c4fa56b4d2401dc8aa88840317b13b5b8
Asmitapaudel <asmeetapdl@gmail.com>
2025-05-20 08:56:44 +0545

    NRS to EURO

---
+++ NodeJs/Assignment/converter.js
@@ -0,0 +1,7 @@
+const nep_euro=(nepali)=>
+{
+
+    return nepali*0.0065;
+}
+
+module.exports={nep_euro}
---
+++ NodeJs/Assignment/task1.js
@@ -0,0 +1,3 @@
+//convert nepali rupee into euro
+const nep_euro=require("./converter")
+console.log(nep_euro.nep_euro(3000))
